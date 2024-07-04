d59b311c4acbfbc00aa2ee55de2b9cb1a7d2eae2
GaneisSapanca <127754886+GaneisSapanca@users.noreply.github.com>
2024-07-04 11:34:48 +0800

    Add files via upload
    Tugas Docker

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profil</title>
+    <style>
+        body {
+            text-align: center;
+            font-family: Arial, sans-serif;
+        }
+        h1, h2 {
+            font-weight: bold;
+        }
+        .profile-info {
+            display: inline-block;
+            text-align: left;
+            margin-top: 20px;
+            font-family: 'Georgia', serif;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        echo "<h1>TUGAS DOCKER</h1>";
+        echo "<h2>Sistem Terdistribusi</h2>";
+    ?>
+    <img src="fotoganis.jpg" alt="Foto Kadek Ganeis Jayarani Sapanca" width="200"><br>
+    <div class="profile-info">
+        <p>Nama : Kadek Ganeis Jayarani Sapanca<br>
+        NIM : 2215101026<br>
+        Kelas : A<br><br>
+        Saya merupakan mahasiswa Universitas Pendidikan Undiksha yang menempuh pendidikan pada prodi S1 Ilmu Komputer</p>
+    </div>
+</body>
+</html>
